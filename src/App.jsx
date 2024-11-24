437033a1e1178b1c9e4554a51b73cc49ea2fe3ec
ctolvett <carlos.tolvett@gmail.com>
2024-11-24 15:44:54 -0300

    Add Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <HeroSection />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
     </>
   );
 };
